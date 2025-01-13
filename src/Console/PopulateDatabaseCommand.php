e08afb4a5bdd6817fd2039a7b58a32f55d5052d3
Math.dlcrt <mathias.delcourt4@etu.univ-lorraine.fr>
2025-01-13 16:58:41 +0100

    Ajout de Faker dans le fichier Populate DB pour la génération de données

--- src/Console/PopulateDatabaseCommand.php
+++ src/Console/PopulateDatabaseCommand.php
@@ -10,6 +10,7 @@ use Slim\App;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Faker\Factory;
 
 class PopulateDatabaseCommand extends Command
 {
@@ -40,34 +41,74 @@ class PopulateDatabaseCommand extends Command
         $db->getConnection()->statement("TRUNCATE `companies`");
         $db->getConnection()->statement("SET FOREIGN_KEY_CHECKS=1");
 
-
-        $db->getConnection()->statement("INSERT INTO `companies` VALUES
-    (1,'Stack Exchange','0601010101','stack@exchange.com','https://stackexchange.com/','https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Verisure_information_technology_department_at_Ch%C3%A2tenay-Malabry_-_2019-01-10.jpg/1920px-Verisure_information_technology_department_at_Ch%C3%A2tenay-Malabry_-_2019-01-10.jpg', now(), now(), null),
-    (2,'Google','0602020202','contact@google.com','https://www.google.com','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Google_office_%284135991953%29.jpg/800px-Google_office_%284135991953%29.jpg?20190722090506',now(), now(), null)
-        ");
-
-        $db->getConnection()->statement("INSERT INTO `offices` VALUES
-    (1,'Bureau de Nancy','1 rue Stanistlas','Nancy','54000','France','nancy@stackexchange.com',NULL,1, now(), now()),
-    (2,'Burea de Vandoeuvre','46 avenue Jeanne d\'Arc','Vandoeuvre','54500','France',NULL,NULL,1, now(), now()),
-    (3,'Siege sociale','2 rue de la primatiale','Paris','75000','France',NULL,NULL,2, now(), now()),
-    (4,'Bureau Berlinois','192 avenue central','Berlin','12277','Allemagne',NULL,NULL,2, now(), now())
-        ");
-
-        $db->getConnection()->statement("INSERT INTO `employees` VALUES
-     (1,'Camille','La Chenille',1,'camille.la@chenille.com',NULL,'Ingénieur', now(), now()),
-     (2,'Albert','Mudhat',2,'albert.mudhat@aqume.net',NULL,'Superviseur', now(), now()),
-     (3,'Sylvie','Tesse',3,'sylive.tesse@factice.local',NULL,'PDG', now(), now()),
-     (4,'John','Doe',4,'john.doe@generique.org',NULL,'Testeur', now(), now()),
-     (5,'Jean','Bon',1,'jean@test.com',NULL,'Developpeur', now(), now()),
-     (6,'Anais','Dufour',2,'anais@aqume.net',NULL,'DBA', now(), now()),
-     (7,'Sylvain','Poirson',3,'sylvain@factice.local',NULL,'Administrateur réseau', now(), now()),
-     (8,'Telma','Thiriet',4,'telma@generique.org',NULL,'Juriste', now(), now())
-        ");
+        $faker = Factory::create('fr_FR');
+        
+        for ($i = 1; $i <= 4; $i++) {
+            $this->createCompany($i, $faker, $db);
+        }
+        $count = 1;
+        for ($i = 1; $i <= 4; $i++) {
+            for ($j = 1; $j <= 2; $j++) {
+                $this->createOffices($count++, $i, $faker, $db);
+            }
+        }
+        $count = 1;
+        for ($i = 1; $i <= 8; $i++) {
+            $this->createEmployees($count++, $i, $faker, $db);
+        }
+        
 
         $db->getConnection()->statement("update companies set head_office_id = 1 where id = 1;");
         $db->getConnection()->statement("update companies set head_office_id = 3 where id = 2;");
 
         $output->writeln('Database created successfully!');
         return 0;
+    }
+
+    private function createCompany($id, $faker, $db): void
+    {
+        $insert = "INSERT INTO `companies` VALUES ";
+        $name = $faker->company;
+        $phone = $faker->phoneNumber;
+        $email = $faker->companyEmail;
+        $website = $faker->url;
+        $image = $faker->imageUrl(800, 600, 'business');
+        $createdAt = $faker->dateTimeThisDecade->format('Y-m-d H:i:s');
+        $updatedAt = $faker->dateTimeThisDecade->format('Y-m-d H:i:s');
+        $insert .= "($id, '$name', '$phone', '$email', '$website', '$image', '$createdAt', '$updatedAt', null)";
+
+        $db->getConnection()->statement($insert);
+    }
+
+    private function createOffices($id, $companyId, $faker, $db): void
+    {
+        $insert = "INSERT INTO `offices` VALUES ";
+        $name = $faker->company . ' Office';
+        $address = $faker->streetAddress;
+        $city = $faker->city;
+        $zipCode = $faker->postcode;
+        $country = $faker->country;
+        $email = $faker->optional()->companyEmail;
+        $phone = $faker->optional()->phoneNumber;
+        $createdAt = $faker->dateTimeThisDecade->format('Y-m-d H:i:s');
+        $updatedAt = $faker->dateTimeThisDecade->format('Y-m-d H:i:s');
+        $insert .= "($id, '$name', '$address', '$city', '$zipCode', '$country', '$email', '$phone', $companyId, '$createdAt', '$updatedAt')";
+
+        $db->getConnection()->statement($insert);
+    }
+
+    private function createEmployees($id, $officeId, $faker, $db): void
+    {
+        $insert = "INSERT INTO `employees` VALUES ";
+        $firstName = $faker->firstName;
+        $lastName = $faker->lastName;
+        $email = $faker->optional()->companyEmail;
+        $phone = $faker->optional()->phoneNumber;
+        $jobTitle = $faker->jobTitle;
+        $createdAt = $faker->dateTimeThisDecade->format('Y-m-d H:i:s');
+        $updatedAt = $faker->dateTimeThisDecade->format('Y-m-d H:i:s');
+        $insert .= "($id, '$firstName', '$lastName', $officeId, '$email', '$phone', \"$jobTitle\", '$createdAt', '$updatedAt')";
+
+        $db->getConnection()->statement($insert);
     }
 }
